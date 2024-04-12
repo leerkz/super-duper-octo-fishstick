04f9d899c5a6d82fb679f73dae5166fd6a80fdfa
Student Free <l3ra131007@gmail.com>
2024-04-12 15:03:01 +0300

    create a function, which sorted data

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,11 @@
-def card_masks(card_number: str | int) -> str:
+def card_masks(card_number: str) -> str:
     """
     Функция принимает на вход номер карты и возвращает ее маску.
     """
     return str(card_number)[:4] + " " + str(card_number)[4:6] + "** **** " + str(card_number)[-4:]
 
 
-def account_masks(account_number: str | int) -> str:
+def account_masks(account_number: str) -> str:
     """
     Функция принимает на вход номер счета и возвращает его маску.
     """
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,11 @@
+from src.masks import card_masks, account_masks
+
+
 def card_account(card: str) -> str:
+    """
+    Возвращает исходную строку с замаскированным номером карты/счета.
+
+    """
     alpha = ""
     digit = ""
     for symbol in card:
@@ -6,3 +13,24 @@ def card_account(card: str) -> str:
             alpha += symbol
         elif symbol.isdigit():
             digit += symbol
+    if alpha.strip() == "Счет":
+
+        return alpha.strip() + " " + account_masks(digit)
+    else:
+
+        return alpha.strip() + " " + card_masks(digit)
+
+
+def date(data_date: str) -> str:
+    """
+    Фильтрует дату.
+
+    """
+    list_data = data_date.split("-")
+    list_data[-1] = list_data[-1].split("T")[0]
+    return ".".join(list_data[::-1])
+
+
+print(date('2018-07-11T02:26:18.671407'))
+print(card_account('Visa Platinum 7000 7922 8960 6361'))
+
