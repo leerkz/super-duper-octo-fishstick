ba08acc512dca05e8cb933fe8e3a78e78601c4f4
Student Free <l3ra131007@gmail.com>
2024-04-15 18:42:18 +0300

    add functions

---
+++ src/processing.py
@@ -0,0 +1,19 @@
+def leaves_dict(dict_list: list, state: str = 'EXECUTED') -> list:
+    """
+    оставляет те словари, которые задал пользователь.
+
+    """
+    new_list = []
+    for item in dict_list:
+        if item['state'] == state:
+            new_list.append(item)
+    return new_list
+
+
+def sorted_dict_date(dict_list: list, reverse: bool = False) -> list:
+    """
+    сортирует словари по дате.
+
+    """
+    new_list = sorted(dict_list, key=lambda x: x['date'], reverse=reverse)
+    return new_list
